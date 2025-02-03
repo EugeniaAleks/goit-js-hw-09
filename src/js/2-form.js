f1ccc4bf83ac745cd73ea13382303e518d920e71
EugeniaAleks <yivga.aleksandrova@gmail.com>
2025-02-03 13:31:59 +0100

    fixed)))

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,49 +5,62 @@ const storageKey = 'feedback-form-state';
 const formData = {
   email: '',
   message: '',
-  toReload() {
-    this.email = '';
-    this.message = '';
-  },
 };
+
+console.log(formData);
 
 const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('input');
 const textareaEl = document.querySelector('textarea');
-
-formEl.addEventListener('input', handlInput);
-
-formEl.addEventListener('submit', handlSabmit);
 toNotReload();
 
+formEl.addEventListener('input', handlInput);
+formEl.addEventListener('submit', handlSabmit);
+
 function handlInput(event) {
-  if (event.target === inputEl) {
-    formData.email = event.target.value.trim();
+  const currentEvent = event.currentTarget.elements;
+
+  if (currentEvent.email && currentEvent.email.value !== '') {
+    formData.email = currentEvent.email.value.trim();
   }
 
-  if (event.target === textareaEl) {
-    formData.message = event.target.value.trim();
+  if (currentEvent.email && currentEvent.message.value !== '') {
+    formData.message = currentEvent.message.value.trim();
   }
+
+  console.log(formData);
 
   localStorage.setItem(storageKey, JSON.stringify(formData));
 }
 
 function toNotReload() {
-  const savedMessage = localStorage.getItem(storageKey);
+  if (localStorage.getItem(storageKey) !== null) {
+    const savedMessage = localStorage.getItem(storageKey);
 
-  const parsedMessage = JSON.parse(savedMessage);
+    const parsedMessage = JSON.parse(savedMessage);
 
-  inputEl.value = parsedMessage.email;
-  textareaEl.value = parsedMessage.message;
+    inputEl.value = parsedMessage.email;
+    textareaEl.value = parsedMessage.message;
+
+    formData.email = inputEl.value;
+    formData.message = textareaEl.value;
+  } else {
+    return;
+  }
 }
 
 function handlSabmit(event) {
   event.preventDefault();
+
   if (inputEl.value === '' || textareaEl.value === '') {
     alert('Fill please all fields');
   } else {
-    console.log(formData);
-    event.currentTarget.reset();
+    console.log('befor:', formData);
+    const currentEvent = event.currentTarget;
+    currentEvent.reset();
     localStorage.removeItem(storageKey);
+    formData.email = currentEvent.elements.email.value;
+    formData.message = currentEvent.elements.message.value;
+    console.log('after:', formData);
   }
 }
